5a51db5c9a76192f1cd22989126a650c51046e3e
keco185 <keco185@me.com>
2015-03-22 11:31:36 -0400

    LEDs only turn on during matches

--- src/org/team484/doge/subsystems/LEDs.java
+++ src/org/team484/doge/subsystems/LEDs.java
@@ -11,48 +11,52 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class LEDs extends Subsystem {
-    
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
 	int blink = 0;
 	int pulse = 0;
 	boolean go = false;
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    	Robot.LEDs.setDirection(Direction.kForward);
-    	setDefaultCommand(new RunLEDs());
-    }
-    public void run() {
-    	if (Robot.ds.isAutonomous()) {
-    		solid();
-    	} else if (Robot.ds.isOperatorControl()) {
-    		if (Robot.ds.getMatchTime() < 20 && Robot.ds.getMatchTime() > 0) {
-    			blink();
-    			if (Robot.ds.getMatchTime() < 5) {
-    				blink++;
-    			}
-    		} else {
-    			solid();
-    		}
-    	}
-    }
-    public void solid() {
-    	Robot.LEDs.set(Value.kOn);
-    }
-    public void blink() {
-    	if (blink < 25) {
-    		Robot.LEDs.set(Value.kOn);
-    		blink++;
-    	} else if (blink < 40) {
-    		Robot.LEDs.set(Value.kOff);
-    		blink++;
-    	} else {
-    		blink = 0;
-    	}
-    }
-    public void off() {
-    	Robot.LEDs.set(Value.kOff);
-    }
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		//setDefaultCommand(new MySpecialCommand());
+		Robot.LEDs.setDirection(Direction.kForward);
+		setDefaultCommand(new RunLEDs());
+	}
+	public void run() {
+		if (Robot.ds.isFMSAttached()) {
+			if (Robot.ds.isAutonomous()) {
+				solid();
+			} else if (Robot.ds.isOperatorControl()) {
+				if (Robot.ds.getMatchTime() < 20 && Robot.ds.getMatchTime() > 0) {
+					blink();
+					if (Robot.ds.getMatchTime() < 5) {
+						blink++;
+					}
+				} else {
+					solid();
+				}
+			}
+		} else {
+			off();
+		}
+	}
+	public void solid() {
+		Robot.LEDs.set(Value.kOn);
+	}
+	public void blink() {
+		if (blink < 25) {
+			Robot.LEDs.set(Value.kOn);
+			blink++;
+		} else if (blink < 40) {
+			Robot.LEDs.set(Value.kOff);
+			blink++;
+		} else {
+			blink = 0;
+		}
+	}
+	public void off() {
+		Robot.LEDs.set(Value.kOff);
+	}
 }
 
